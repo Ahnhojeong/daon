8c7c23ea1d00d527a963d327ee5e681b4e8b9746
Ahnhojeong <77098852+Ahnhojeong@users.noreply.github.com>
2021-05-02 22:19:35 +0900

    daon_complete
    1. 패럴렉스 효과 수정
    2. 모바일 반응형 수정
    3. 번들 진행

--- js/about.js
+++ js/about.js
@@ -1,17 +1,1 @@
-
-$(function () {
-    $(window).scroll(function () {
-        var position = $(this).scrollTop();
-        $('.imgHolder img').css({ top: position / 2 });
-    });
-
-    $('.title').on('click', '', function () {
-        var top = $('section').first().position().top;
-        $("html, body").animate({
-            scrollTop: top
-        }, {
-            easing: "swing", duration: 800
-        });
-    });
-
-});
+$((function(){$(window).scroll((function(){var o=$(this).scrollTop();$(".imgHolder img").css({top:o/2})})),$(".title").on("click","",(function(){var o=$("section").first().position().top;$("html, body").animate({scrollTop:o},{easing:"swing",duration:800})}))}));
